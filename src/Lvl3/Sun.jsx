02bb37d5dd6eeac8e9a513b94c58b07180f55579
Muhammed <momosuli8@gmail.com>
2023-10-01 18:40:41 +0200

    Added Sun Game over

--- src/Lvl3/Sun.jsx
+++ src/Lvl3/Sun.jsx
@@ -1,13 +1,15 @@
-import React, { useRef } from "react";
+import React, { useRef, useState} from "react";
 import { useGLTF, useTexture } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { Vector3 } from "three";
 import { planePosition } from "./Lvl3Spaceship";
+import { displayGameOver } from "../GameOver";
 
 export function Sun(props) {
   const { nodes, materials } = useGLTF("assets/models/Sun.glb");
 
   const [sunTexture] = useTexture(['/assets/textures/sun.jpg'])
+  const [gameOver, setGameOver] = useState(false);
 
   // Define the center and radius of the green sphere
   const sphereCenter = new Vector3(0, 0, 0);
@@ -20,11 +22,19 @@ export function Sun(props) {
   useFrame(() => {
     // Calculate the distance between the plane position and the sphere center
     const distance = planePosition.distanceTo(sphereCenter);
-    console.log(planePosition)
 
     // Check if the plane is inside the sphere
-    if (distance < sphereRadius) {
-      console.log("Collision detected!");
+    if (distance < sphereRadius && !gameOver) {
+      console.log("Game Over!");
+      const leaderboardData = [
+        { name: "Sayf", timeLasted: "1 second" },
+        { name: "Muz", timeLasted: "180 seconds" },
+        { name: "Daggy", timeLasted: "90 seconds" }
+    ];
+      setGameOver(true);
+      //Msg For Game over Reason
+      const message = "You went into the sun... BRUH";
+      displayGameOver(leaderboardData,message);
     }
   });
 
