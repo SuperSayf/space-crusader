6e09e1436d36b7578dbda595cee073f7e0f67f07
Sayfullah <sayfullah.jumoorty@live.com>
2023-10-16 17:00:22 +0200

    Game Menu Change

--- src/main.jsx
+++ src/main.jsx
@@ -1,42 +1,91 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useState } from "react";
 import ReactDOM from "react-dom/client";
 import Lvl1 from "./Lvl1/Lvl1.jsx";
 import Lvl2 from "./Lvl2/Lvl2.jsx";
 import Lvl3 from "./Lvl3/Lvl3.jsx";
 import { Canvas } from "@react-three/fiber";
 import "./index.css";
-import { Loader } from "@react-three/drei";
+import { Html } from "@react-three/drei";
 
 const rootContainer = document.getElementById("root");
-//Level Parameter
-const urlParams = new URLSearchParams(window.location.search);
-const selectedLevel = urlParams.get("level");
 
-//Dynamic Level selection Logic
-let LevelComponent;
-if (selectedLevel === "lvl1") {
-  LevelComponent = Lvl1;
-} else if (selectedLevel === "lvl2") {
-  LevelComponent = Lvl2;
-} else if (selectedLevel === "lvl3") {
-  LevelComponent = Lvl3;
-} else {
-  LevelComponent = Lvl1; // Default to Lvl1 if no valid level is provided
-}
+const App = () => {
+  const [start, setStart] = useState(false);
+  const [selectedLevel, setSelectedLevel] = useState("lvl1");
 
-if (rootContainer) {
-  ReactDOM.createRoot(rootContainer).render(
+  const handleNewGameClick = () => {
+    setSelectedLevel("lvl1");
+    setStart(true);
+  };
+
+  const handleLevelSelection = (level) => {
+    setSelectedLevel(level);
+    setStart(true);
+  };
+
+  return (
     <>
       <Canvas shadows>
-        <color attach="background" args={["black"]} />
+        <color attach="background" args={["white"]} />
         <Suspense fallback={null}>
-          {/* <Lvl3 /> */}
-          <LevelComponent />
+          {selectedLevel === "lvl1" && start ? <Lvl1 /> : null}
+          {selectedLevel === "lvl2" && start ? <Lvl2 /> : null}
+          {selectedLevel === "lvl3" && start ? <Lvl3 /> : null}
+          {!start && (
+            <Html>
+              <>
+                <header>
+                  <h1>Space Crusaders</h1>
+                </header>
+                <ul className="threeD-button-set">
+                  <li>
+                    <button id="newGameButton" onClick={handleNewGameClick}>
+                      New Game
+                    </button>
+                  </li>
+                  <li>
+                    <button id="levelButton">Levels</button>
+                    <ul className="level-options">
+                      <li>
+                        <button onClick={() => handleLevelSelection("lvl1")}>
+                          Level 1
+                        </button>
+                      </li>
+                      <li>
+                        <button onClick={() => handleLevelSelection("lvl2")}>
+                          Level 2
+                        </button>
+                      </li>
+                      <li>
+                        <button onClick={() => handleLevelSelection("lvl3")}>
+                          Level 3
+                        </button>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <button>How to Play</button>
+                  </li>
+                  <li>
+                    <button>Credits</button>
+                  </li>
+                  <li>
+                    <button>Quit</button>
+                  </li>
+                </ul>
+              </>
+            </Html>
+          )}
         </Suspense>
       </Canvas>
-      {/* <Loader /> */}
     </>
   );
+};
+
+if (rootContainer) {
+  ReactDOM.createRoot(rootContainer).render(<App />);
 } else {
   console.error("Root container element not found.");
 }
+
+export default App;
--- src/startScreen/index.js
+++ src/startScreen/index.js
@@ -1,24 +1,76 @@
-const menu = document.querySelector(".threeD-button-set");
-const menuRect = menu.getBoundingClientRect();
+import React, { useEffect } from "react";
 
-const { matches: motionOK } = window.matchMedia(
-  "(prefers-reduced-motion: no-preference)"
-);
+const GameMenu = () => {
+  const handleNewGameClick = () => {
+    window.location.href = "game.html";
+  };
 
-if (motionOK) {
-  window.addEventListener("mousemove", ({ target, clientX, clientY }) => {
-    const { dx, dy } = getAngles(clientX, clientY);
+  const handleLevelButtonClick = () => {
+    window.location.href = "level.html";
+  };
 
-    menu.style.setProperty("--x", `${dy / 20}deg`);
-    menu.style.setProperty("--y", `${dx / 20}deg`);
-  });
-}
+  useEffect(() => {
+    const menu = document.querySelector(".threeD-button-set");
+    const menuRect = menu.getBoundingClientRect();
 
-const getAngles = (clientX, clientY) => {
-  const { x, y, width, height } = menuRect;
+    const { matches: motionOK } = window.matchMedia(
+      "(prefers-reduced-motion: no-preference)"
+    );
 
-  const dx = clientX - (x + 0.5 * width);
-  const dy = clientY - (y + 0.5 * height);
+    if (motionOK) {
+      const getAngles = (clientX, clientY) => {
+        const { x, y, width, height } = menuRect;
 
-  return { dx, dy };
+        const dx = clientX - (x + 0.5 * width);
+        const dy = clientY - (y + 0.5 * height);
+
+        return { dx, dy };
+      };
+
+      const handleMouseMove = ({ target, clientX, clientY }) => {
+        const { dx, dy } = getAngles(clientX, clientY);
+
+        menu.style.setProperty("--x", `${dy / 20}deg`);
+        menu.style.setProperty("--y", `${dx / 20}deg`);
+      };
+
+      window.addEventListener("mousemove", handleMouseMove);
+
+      return () => {
+        // Clean up the event listener when the component unmounts
+        window.removeEventListener("mousemove", handleMouseMove);
+      };
+    }
+  }, []);
+
+  return (
+    <div>
+      <header>
+        <h1>Space Crusaders</h1>
+      </header>
+      <ul className="threeD-button-set">
+        <li>
+          <button id="newGameButton" onClick={handleNewGameClick}>
+            New Game
+          </button>
+        </li>
+        <li>
+          <button id="levelButton" onClick={handleLevelButtonClick}>
+            Levels
+          </button>
+        </li>
+        <li>
+          <button>How to Play</button>
+        </li>
+        <li>
+          <button>Credits</button>
+        </li>
+        <li>
+          <button>Quit</button>
+        </li>
+      </ul>
+    </div>
+  );
 };
+
+export default GameMenu;
