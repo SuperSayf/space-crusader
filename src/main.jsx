8c497fa0672bbd8e1c5b1b89fc7eec4c1830d5a4
Sayfullah <sayfullah.jumoorty@live.com>
2023-10-26 01:46:59 +0200

    new game lore

--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,45 @@
-import React, { Suspense, useState, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import ReactDOM from "react-dom/client";
-import Lvl1 from "./Lvl1/Lvl1.jsx";
-import Lvl2 from "./Lvl2/Lvl2.jsx";
-import Lvl3 from "./Lvl3/Lvl3.jsx";
 import { Canvas } from "@react-three/fiber";
 import "./index.css";
 import "./startScreen/game-menu.css";
 import { Html } from "@react-three/drei";
+
 const rootContainer = document.getElementById("root");
 
 const App = () => {
   const [start, setStart] = useState(false);
-  const [selectedLevel, setSelectedLevel] = useState("lvl1");
-  const [showLevelOptions, setShowLevelOptions] = useState(false); // New state
-
-  // Check for the level parameter in the URL and set it as the initial selected level
-  useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const levelParam = urlParams.get("level");
-    if (levelParam) {
-      setSelectedLevel(levelParam);
-      setStart(true);
-    }
-  }, []);
+  const [showVideo, setShowVideo] = useState(false);
+  const [showLevelOptions, setShowLevelOptions] = useState(false);
+  const videoRef = useRef(null);
 
   const handleLevelsButtonClick = () => {
-    setShowLevelOptions(!showLevelOptions); // Toggle level options visibility
+    setShowLevelOptions(!showLevelOptions);
   };
 
   const handleCreditsButtonClick = () => {
-    // Change the window.location.href
     window.location.href = "credits.html";
   };
 
+  const handleNewGameClick = () => {
+    setShowVideo(true);
+    setStart(true);
+
+    // Play the "lore.mp4" video
+    const videoElement = videoRef.current;
+    if (videoElement) {
+      videoElement.style.display = "block"; // Show the video
+      videoElement.play();
+
+      // After playing the video, navigate to level 1
+      videoElement.onended = () => {
+        window.location.href = "lvl1.html";
+      };
+    }
+  };
+
   const handleLvl1Click = () => {
+    // Navigate to level 1 directly without playing the video
     window.location.href = "lvl1.html";
   };
 
@@ -49,12 +55,33 @@ const App = () => {
     <div className="container">
       <div className="video-background">
         <video
-          playsInline="playsInline"
-          autoPlay="autoplay"
-          muted="muted"
-          loop="loop"
+          playsInline
+          autoPlay
+          muted
+          loop
+          style={{ width: "100%", height: "100%" }}
         >
           <source src="assets/audio/backgroundvideo.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <div
+        className="video-lore"
+        style={{
+          display: showVideo ? "block" : "none",
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <video
+          ref={videoRef}
+          playsInline
+          style={{ width: "100%", height: "100%" }}
+        >
+          <source src="assets/audio/lore-full-c.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
@@ -65,7 +92,7 @@ const App = () => {
           </header>
           <ul className="threeD-button-set">
             <li>
-              <button id="newGameButton" onClick={handleLvl1Click}>
+              <button id="newGameButton" onClick={handleNewGameClick}>
                 New Game
               </button>
             </li>
@@ -73,7 +100,7 @@ const App = () => {
               <button id="levelButton" onClick={handleLevelsButtonClick}>
                 Levels
               </button>
-              {showLevelOptions && ( // Show level options if showLevelOptions is true
+              {showLevelOptions && (
                 <ul className="level-options">
                   <li>
                     <button onClick={handleLvl1Click}>Level 1</button>
